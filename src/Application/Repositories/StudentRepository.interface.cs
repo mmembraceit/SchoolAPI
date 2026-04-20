d8ad9431d6a4a03bf647c64e3992d09c11f15176
mmembraceit <mmb@embrace-it.com>
2026-04-20 10:03:17 +0200

    feat(application): change to IReadOnlyList for repository use

--- src/Application/Repositories/StudentRepository.interface.cs
+++ src/Application/Repositories/StudentRepository.interface.cs
@@ -5,7 +5,7 @@ namespace StudentApi.Application.Repositories;
 public interface IStudentRepository
 {
     Task<StudentModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-    Task<IEnumerable<StudentModel>> GetAllAsync(CancellationToken cancellationToken);
+    Task<IReadOnlyList<StudentModel>> GetAllAsync(CancellationToken cancellationToken);
     Task AddAsync(StudentModel student, CancellationToken cancellationToken);
     Task UpdateAsync(StudentModel student, CancellationToken cancellationToken);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken);
--- src/Application/Repositories/TenantRepository.interface.cs
+++ src/Application/Repositories/TenantRepository.interface.cs
@@ -5,7 +5,7 @@ namespace StudentApi.Application.Repositories;
 public interface ITenantRepository
 {
     Task<TenantModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-    Task<IEnumerable<TenantModel>> GetAllAsync(CancellationToken cancellationToke);
+    Task<IReadOnlyList<TenantModel>> GetAllAsync(CancellationToken cancellationToken);
     Task AddAsync(TenantModel tenant, CancellationToken cancellationToken);
     Task UpdateAsync(TenantModel tenant, CancellationToken cancellationToken);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken);
