c83288e86527d90f61a9b228d78693305b9f9fe3
mmembraceit <mmb@embrace-it.com>
2026-04-20 14:28:24 +0200

    feat(infra): Student.TenantId migration

---
+++ src/Infrastructure/Migrations/20260420122445_AddStudentTenantId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentTenantId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "students",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "students");
+        }
+    }
+}
