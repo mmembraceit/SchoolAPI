a3839e05b36de0e665d1473da0fcdfb6194cb6cd
mmembraceit <mmb@embrace-it.com>
2026-04-20 09:08:07 +0200

    feat(application): Student Service inteface

---
+++ src/Application/Services/StudentService.interface.cs
@@ -0,0 +1,12 @@
+using StudentApi.Application.DTOs.Students;
+
+namespace StudentApi.Application.Services;
+
+public interface IStudentService
+{
+    Task<StudentResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IEnumerable<StudentResponse>> GetAllAsync(CancellationToken cancellationToken);
+    Task<StudentResponse> CreateAsync(StudentCreateRequest request, CancellationToken cancellationToken);
+    Task<StudentResponse?> UpdateAsync(Guid id, StudentUpdateRequest request, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
