bc86ee3aacdf5dd68f1cc8676dc007d4b19d2f07
mmembraceit <mmb@embrace-it.com>
2026-04-21 08:17:45 +0200

    feature(application) student and tenant service interfaces updated

--- src/Application/Services/StudentService.interface.cs
+++ src/Application/Services/StudentService.interface.cs
@@ -4,9 +4,9 @@ namespace StudentApi.Application.Services;
 
 public interface IStudentService
 {
-    Task<StudentResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<StudentResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken);
     Task<IEnumerable<StudentResponse>> GetAllAsync(CancellationToken cancellationToken);
     Task<StudentResponse> CreateAsync(StudentCreateRequest request, CancellationToken cancellationToken);
-    Task<StudentResponse?> UpdateAsync(Guid id, StudentUpdateRequest request, CancellationToken cancellationToken);
-    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+    Task<StudentResponse> UpdateAsync(Guid id, StudentUpdateRequest request, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
 }
--- src/Application/Services/TenantService.interface.cs
+++ src/Application/Services/TenantService.interface.cs
@@ -4,9 +4,9 @@ namespace StudentApi.Application.Services;
 
 public interface ITenantService
 {
-    Task<TenantResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<TenantResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken);
     Task<IEnumerable<TenantResponse>> GetAllAsync(CancellationToken cancellationToken);
     Task<TenantResponse> CreateAsync(TenantCreateRequest request, CancellationToken cancellationToken);
-    Task<TenantResponse?> UpdateAsync(Guid id, TenantUpdateRequest request, CancellationToken cancellationToken);
-    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+    Task<TenantResponse> UpdateAsync(Guid id, TenantUpdateRequest request, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
 }
