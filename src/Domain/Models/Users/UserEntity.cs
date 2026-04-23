6e97813369c76ee2cd98332b73e9263717c4abba
mmembraceit <mmb@embrace-it.com>
2026-04-23 08:56:06 +0200

    feat(domain): DB Seeding - new UserEntity

---
+++ src/Domain/Models/Users/UserEntity.cs
@@ -0,0 +1,8 @@
+namespace StudentApi.Domain.Models.Users;
+
+public class UserEntity : BaseEntity
+{
+    public Guid TenantId { get; set; }
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+}
