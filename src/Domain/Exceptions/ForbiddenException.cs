296fdf83579c060b21ae9374941615141ee96b7a
mmembraceit <mmb@embrace-it.com>
2026-04-24 13:16:36 +0200

    feature(domain): exception unused deleted

--- src/Domain/Exceptions/ForbiddenException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace StudentApi.Domain.Exceptions;
-
-public sealed class ForbiddenException : ApiException
-{
-    public ForbiddenException(string errorCode, string message)
-        : base(HttpStatusCode.Forbidden, errorCode, message) { }
-}
