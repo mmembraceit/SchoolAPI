71f7b452ae3aa4142ad531fa4c61149a22eea9d7
mmembraceit <mmb@embrace-it.com>
2026-04-24 12:37:02 +0200

    feature(domain): Student error code added

--- src/Domain/ErrorCodes/StudentApiErrorCodes.Student.cs
+++ src/Domain/ErrorCodes/StudentApiErrorCodes.Student.cs
@@ -5,5 +5,6 @@ public static partial class StudentApiErrorCodes
     public static class Student
     {
         public const string NotFound = "error.student.not_found";
+        public const string NameAlreadyExists = "error.student.name_already_exists";
     }
 }
